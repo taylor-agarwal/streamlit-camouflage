9a98a2b5c1da26940bc2b177afaf4598dad85d03
Taylor Agarwal <taylor.agarwal@gmail.com>
2024-01-15 20:56:01 -0500

    Update UI for better UX

--- webapp/Camouflage.py
+++ webapp/Camouflage.py
@@ -63,90 +63,65 @@ st.write(TITLE_HTML, unsafe_allow_html=True)
 
 # User select number of images
 num_images = st.selectbox("Number of Clothing Items", CLOTHING_NUMBER_CHOICES, on_change=user_activity, args=("NUMBER INPUT - Number of items changed",))
+system_activity(f"NUMBER INPUT - {num_images} items selected")
 
-# Generate camera input dialogues
-images = []
+tabs = None
 if int(num_images) > 0:
-    system_activity(f"NUMBER INPUT - {num_images} items selected")
-    num_images = int(num_images)
-    image_col1, image_col2 = st.columns(2)
-    for i in range(num_images):
-        if i % 2 == 0:
-            col = image_col1
-        else:
-            col = image_col2
-        with col:
-            image = st.camera_input(f"image-{i+1}", label_visibility="hidden", on_change=user_activity, args=(f"IMAGE CAPTURE - {i+1} - Image changed",))
-        images.append(image)
+    tabs = st.tabs([f"Item {i+1}" for i in range(num_images)])
 
-# If all the images have been taken, start processing them
-images_taken = (len(images) > 0) and (all([image is not None for image in images]))
-images_rembg = []
-if images_taken:
-    progress_bar = st.progress(0, "Extracting Clothes...")
-    system_activity("CLOTHING EXTRACTION - All images collected, beginning clothing extraction")
-    with st.expander("View Extracted Clothes"):
-        # Save clothes and remove backgrounds
-        rembg_tabs = st.tabs([f"Image {i+1}" for i in range(len(images))])
-        for i, image in enumerate(images):
-            try:
-                # Remove background from image
-                files = {"file": image}
-                response = requests.post(API_ROUTES["rembg"], files=files, stream=True)
-                response.raise_for_status()
-                im = io.BytesIO(response.content)
-                image_rembg = PILImage.open(im).convert('RGB')
-                images_rembg.append(response.content)
-                with rembg_tabs[i]:
-                    st.image(image_rembg)
-                system_activity(f"CLOTHING EXTRACTION - {i+1} - Extracted clothes from image")
-            except Exception as e:
-                system_error(f"CLOTHING EXTRACTION - {i+1} - Error extracting clothes from images", e)
-                st.error("Unable to extract clothes. Please try again.")
-                st.stop()
+chosen_colors = []
+for i, tab in enumerate(tabs):
+    with tab:
+        # Generate camera input dialogues
+        image = st.camera_input(f"image-{i+1}", label_visibility="hidden", on_change=user_activity, args=(f"IMAGE CAPTURE - {i+1} - Image changed",))
+        image_rembg_bytes = None
+        if image is not None:
+            # Remove background from image
+            files = {"file": image}
+            response = requests.post(API_ROUTES["rembg"], files=files, stream=True)
+            response.raise_for_status()
+            image_rembg_bytes = response.content
+            # im = io.BytesIO(response.content)
+            # image_rembg = PILImage.open(im).convert('RGB')
+            # st.image(image_rembg)
+        if image_rembg_bytes is not None:
+            # Extract and display colors
+            files = {'file': image_rembg_bytes}
+            response = requests.post(API_ROUTES["colors"], files=files, stream=True)
+            response.raise_for_status()
+            colors_json = response.json()
+            system_activity(f"COLOR EXTRACTION - {i+1} - Colors {colors_json}")
+            colors = colors_json['colors']
+            rect = get_color_rect(colors)
+            st.image(rect)
+            st.write(f"Colors: {', '.join([color['name'] for color in colors])}")
+            columns = st.columns(len(colors))
+            colors_hex = [color['hex'] for color in colors]
+            for j, column in enumerate(columns):
+                with column:
+                    hex = colors_hex[j]
+                    st.color_picker(label=hex, value=hex)
+                    default = True if j == 0 else False
+                    picked = st.checkbox(label=f"color_choice_{i}_{j}", value=default, label_visibility="hidden")
+                    if picked:
+                        chosen_colors.append(colors[j])
 
 # TODO: Consider storing colors and only rerunning above lines if the images change - st.session_state colors with callback on images on_change
 # Display the colors from the items
 # https://blog.streamlit.io/create-a-color-palette-from-any-image/ 
-chosen_colors = []
-if len(images_rembg) > 0:
-    progress_bar.progress(50, "Extracting Colors...")
-    system_activity("COLOR EXTRACTION - Extracting colors from all images")
-    with st.expander("View Extracted Colors"):
-        # Extract and save colors
-        color_tabs = st.tabs([f"Image {i+1}" for i in range(len(images))])
-        for i, image in enumerate(images_rembg):
-            try:
-                files = {'file': image}
-                response = requests.post(API_ROUTES["colors"], files=files, stream=True)
-                response.raise_for_status()
-                colors_json = response.json()
-                system_activity(f"COLOR EXTRACTION - {i+1} - Colors {colors_json}")
-                colors = colors_json['colors']
-                rect = get_color_rect(colors)
-                with color_tabs[i]:
-                    st.image(rect)
-                    st.write(f"Colors: {', '.join([color['name'] for color in colors])}")
-                    columns = st.columns(len(colors))
-                    colors_hex = [color['hex'] for color in colors]
-                    for j, column in enumerate(columns):
-                        with column:
-                            hex = colors_hex[j]
-                            st.color_picker(label=hex, value=hex)
-                            default = True if j == 0 else False
-                            picked = st.checkbox(label=f"color_choice_{i}_{j}", value=default, label_visibility="hidden")
-                            if picked:
-                                chosen_colors.append(colors[j])
-            except Exception as e:
-                system_error(f"COLOR EXTRACTION - {i+1} - Error extracting colors from images", e)
-                st.error("Unable to extract clothes. Please try again.")
-                st.stop()
-
 
 # Determine if the colors are a match
 matches = None
 if len(chosen_colors) > 0:
-    progress_bar.progress(90, "Determining match...")
+    width = 500
+    height = 200
+    for c in chosen_colors:
+        c['pct'] = 1 / len(chosen_colors)
+    rect = get_color_rect(colors=chosen_colors, width=width, height=height)
+    _, col, _ = st.columns([1, 4, 1])
+    with col:
+        with st.container(border=True):
+            st.image(rect, use_column_width=True)
     # Check outfit for matches
     with st.spinner("Checking for a match..."):
         try:
@@ -173,8 +148,6 @@ if len(chosen_colors) > 0:
             system_activity("RESULT - No Match")
 
 if matches is not None:
-    progress_bar.progress(100, "Complete!")
-
     # Show feedback link
     with st.container():
         st.write("<a href='https://forms.gle/PTqChvC2sJUB5B6NA'>Give Feedback</a>", unsafe_allow_html=True)
